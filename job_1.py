13377043ee4769a086e8c42604247d9498145aeb
sanjeev4ds <101089685+sanjeev4ds@users.noreply.github.com>
2025-03-24 18:17:32 +0530

    Add files via upload

---
+++ job_1.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.appName("app_1_deploy") \
+                    .getOrCreate()
+					
+df = spark.read.format("csv") \
+    .option("inferschema","true") \
+    .option("header","true") \
+    .load("gs://sapient-sanjeevsaini/input/1MB/customers.csv")
+	
+df_final = df.groupBy("state","city").agg(count_distinct("*").alias("total_customers"))
+
+df_final.write.option("header","true").csv("gs://sapient-sanjeevsaini/output/1MB/customers_final.csv")
