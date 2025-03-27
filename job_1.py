0b8d1beb417c07aa4f17169d46f763fca079477b
sanjeev4ds <101089685+sanjeev4ds@users.noreply.github.com>
2025-03-27 09:44:54 +0530

    Update job_1.py

--- job_1.py
+++ job_1.py
@@ -7,12 +7,13 @@ spark = SparkSession.builder.appName("app_1_deploy") \
 df = spark.read.format("csv") \
     .option("inferschema","true") \
     .option("header","true") \
-    .load("gs://sapient-sanjeevsaini/input/1MB/customers.csv")
+    .load("gs://sanjeev-bucket-name-issue/input/1MB/customers.csv")
 	
 df_final = df.groupBy("state","city").agg(count_distinct("*").alias("total_customers"))
 
 df_final.write \
 .mode("overwrite") \
 .option("header","true") \
-.csv("gs://sapient-sanjeevsaini/output/1MB/customers_final_new.csv")
+.csv("gs://sanjeev-bucket-name-issue/output/1MB/customers_final_new.csv")
+# avoid using exact names in personal account for public repositories
 #adding comment to test workflow re-run
